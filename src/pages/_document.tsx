694d0b6dee963bcf2d4b882b6233b0e0b7bcd811
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2025-09-09 06:05:08 -0300

    add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang='pt-br'>
-      <Head />
-      <body className='bg-black-dark'>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="pt-br">
+			<Head />
+			<script
+				defer
+				src="https://umami-production-e4bd.up.railway.app/script.js"
+				data-website-id="7ab69f6f-d628-445d-810a-7a97388c2bd6"
+			></script>
+			<body className="bg-black-dark">
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
