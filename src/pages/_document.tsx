f6c4c3d88b31db4d6fe62d98e7012c178b975b04
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2025-09-09 06:09:32 -0300

    add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,7 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-	return (
+  return (
 		<Html lang="pt-br">
 			<Head />
 			<script
@@ -14,5 +14,5 @@ export default function Document() {
 				<NextScript />
 			</body>
 		</Html>
-	);
+  )
 }
